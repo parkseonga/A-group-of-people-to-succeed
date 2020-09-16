6030edeee439a073b0cc0f938055cfcbfbb218da
ssung <33725048+parkseonga@users.noreply.github.com>
2020-09-16 21:51:34 +0900

    pratice_day2

---
+++ code_1074, 1721, 2779.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 16 17:05:52 2020
+
+@author: a0105
+"""
+# 1074
+n = int(input())
+
+while n!=0:
+    print(n)
+
+    n-=1
+    if n ==0:
+        break 
+ 
+# 1721
+import math
+
+x1, y1 = map(float,input().split())
+x2, y2 = map(float,input().split())
+
+print("%0.2f" % math.sqrt((x1-x2)**2+(y1-y2)**2))
+
+# 2779
+##
+
